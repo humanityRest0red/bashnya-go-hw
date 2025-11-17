5a2e440ba15fb62964598f02e91c65f0a249be72
Kaseltzer Timeon <kaseltzt@il-f6.msk.21-school.ru>
2025-11-17 21:34:27 +0300

    added context in task2

--- bashnya-hw5/task_2/main.go
+++ bashnya-hw5/task_2/main.go
@@ -1,67 +1,98 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"math/rand/v2"
 	"os"
 	"os/signal"
 	"sync"
+	"time"
 )
 
+//	func foo(cancel context.CancelFunc) {
+//		sigCh := make(chan os.Signal, 1)
+//		signal.Notify(sigCh, os.Interrupt)
+//		go func() {
+//			<-sigCh
+//			fmt.Println("\n\033[31mInterrupt signal received, shutting down...\033[0m")
+//			cancel()
+//		}()
+//	}
 func main() {
-	var workers = 2
-	var wg sync.WaitGroup
-	wg.Add(workers)
-
-	for range workers {
-		go func() {
-			defer wg.Done()
-			ch1 := sendRandomDataToChannel()
-			ch2 := readFromChannel(ch1)
-			outputChannel(ch2)
-		}()
+	workers, err := getWorkersCount()
+	if err != nil {
+		fmt.Print(err)
+		return
 	}
-	wg.Wait()
 
-	fmt.Println("done")
-}
+	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
 
-func sendRandomDataToChannel() chan int {
-	ch := make(chan int)
-
+	// foo(cancel)
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
 	go func() {
 		<-sigCh
-		fmt.Println("\nПолучен сигнал прерывания")
-		// cancel()
+		fmt.Println("\n\033[31mInterrupt signal received, shutting down...\033[0m")
+		cancel()
 	}()
 
-	go func() {
-		defer close(ch)
-		for {
+	dataCh := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Go(func() {
+		sendRandomDataToChannel(ctx, dataCh)
+	})
+
+	for w := range workers {
+		wg.Go(func() {
+			readAndProcess(ctx, dataCh, w+1)
+		})
+	}
+	wg.Wait()
+
+	fmt.Println("All Done... Are you happy now?")
+
+}
+
+func getWorkersCount() (uint, error) {
+	var N uint
+	flag.UintVar(&N, "N", 0, "Workers count [>0]")
+	flag.Parse()
+
+	if N == 0 {
+		return 0, fmt.Errorf("usage: %s -N [>0]", os.Args[0])
+	}
+	return N, nil
+}
+
+func sendRandomDataToChannel(ctx context.Context, ch chan int) {
+	defer close(ch)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
 			ch <- rand.IntN(1000)
 		}
-	}()
-
-	return ch
+	}
 }
 
-func readFromChannel(data_chan chan int) chan int {
-	ch := make(chan int)
-
-	go func() {
-		defer close(ch)
-		for num := range data_chan {
-			ch <- num
+func readAndProcess(ctx context.Context, ch chan int, wId uint) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case num, ok := <-ch:
+			if !ok {
+				fmt.Printf("Worker %v is dead...\n", wId)
+				return
+			}
+			fmt.Printf("Worker %v: %v\n", wId, num)
+			time.Sleep(100 * time.Millisecond)
 		}
-	}()
-
-	return ch
-}
-
-func outputChannel(ch chan int) {
-	for num := range ch {
-		fmt.Println(num)
 	}
 }
