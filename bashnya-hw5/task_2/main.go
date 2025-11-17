b7194a5ffb7954141dbdf7573d6dcea123e4c259
Kaseltzer Timeon <kaseltzt@il-f6.msk.21-school.ru>
2025-11-17 22:15:55 +0300

    refactored task_2

--- bashnya-hw5/task_2/main.go
+++ bashnya-hw5/task_2/main.go
@@ -11,38 +11,14 @@ import (
 	"time"
 )
 
-//	func foo(cancel context.CancelFunc) {
-//		sigCh := make(chan os.Signal, 1)
-//		signal.Notify(sigCh, os.Interrupt)
-//		go func() {
-//			<-sigCh
-//			fmt.Println("\n\033[31mInterrupt signal received, shutting down...\033[0m")
-//			cancel()
-//		}()
-//	}
 func main() {
-	workers, err := getWorkersCount()
-	if err != nil {
-		fmt.Print(err)
-		return
-	}
+	var (
+		workers = mustWorkersCount()
+		ctx     = contextWithSignalInterrupt()
+		dataCh  = make(chan int)
+		wg      sync.WaitGroup
+	)
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	ctx, cancel := context.WithCancel(context.Background())
-	// defer cancel()
-
-	// foo(cancel)
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-	go func() {
-		<-sigCh
-		fmt.Println("\n\033[31mInterrupt signal received, shutting down...\033[0m")
-		cancel()
-	}()
-
-	dataCh := make(chan int)
-
-	var wg sync.WaitGroup
 	wg.Go(func() {
 		sendRandomDataToChannel(ctx, dataCh)
 	})
@@ -55,18 +31,32 @@ func main() {
 	wg.Wait()
 
 	fmt.Println("All Done... Are you happy now?")
-
 }
 
-func getWorkersCount() (uint, error) {
+func mustWorkersCount() uint {
 	var N uint
 	flag.UintVar(&N, "N", 0, "Workers count [>0]")
 	flag.Parse()
 
 	if N == 0 {
-		return 0, fmt.Errorf("usage: %s -N [>0]", os.Args[0])
+		panic(fmt.Sprintf("usage: %s -N [>0]", os.Args[0]))
 	}
-	return N, nil
+
+	return N
+}
+
+func contextWithSignalInterrupt() context.Context {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	go func() {
+		<-sigCh
+		fmt.Println("\n\033[31mInterrupt signal received, shutting down...\033[0m")
+		cancel()
+	}()
+
+	return ctx
 }
 
 func sendRandomDataToChannel(ctx context.Context, ch chan int) {
